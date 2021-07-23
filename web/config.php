52427e6b326fd9a2456cba9d15f5dd5cc8a5c662
candy-echo <87758355+candy-echo@users.noreply.github.com>
2021-07-23 20:16:25 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -8,10 +8,18 @@ $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 // define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 // define('EMAIL', 'None@gmail.com');
 
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+// define('APIKEY', getenv("APIKEY"));
+// define('GJ_CODE', getenv("GJ_CODE"));
+// define('SITE_NAME',getenv("SITE_NAME"));
+// define('TITLENAME', getenv("TITLENAME"));
+// define('EN2DEKEY',getenv("EN2DEKEY"));
+// define('EMAIL', getenv("EMAIL"));
+
+define('ROOT_PART', Root_part());
+define('APIKEY', 'please input your APIKEY');
+define('GJ_CODE', $gl);
+define('SITE_NAME', 'please input your SiteName');
+define('TITLENAME', 'please input your SiteTitle');
+define('EN2DEKEY', 'please input EncryptKey, randome generate');
+define('EMAIL', 'please input your Email');
 ?>
